e2ec1c250cd9b82559f3209d33fbbcf3d02ce034
yamayuz <yuki@yamayulab.com>
2022-04-22 01:25:48 +0900

    [add] how to do

--- chapter3/list3-3.cpp
+++ chapter3/list3-3.cpp
@@ -1,8 +1,23 @@
+#include <ctime>
+#include <cstdlib>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "test";
+    int x;
+
+    do {
+        cout << "1～10の整数を入力してください：";
+        cin >> x;
+
+        if (cin.fail()) {
+            cout << "整数を入力してください！\n";
+            cin.clear();
+            cin.ignore(1024, '\n');
+        }
+    } while (x < 1 || x > 10);
+
+    cout << "入力値は" << x << "です。\n";
 }
