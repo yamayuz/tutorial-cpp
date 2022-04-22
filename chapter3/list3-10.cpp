a30b364e9ae2154dfafb4d4798f5d1cb7d0c42c0
yamayuz <yuki@yamayulab.com>
2022-04-22 19:06:36 +0900

    [add] how to do and while

---
+++ chapter3/list3-10.cpp
@@ -0,0 +1,22 @@
+// 1からnまでの和を求める
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "1からnまでの和を求めます。\n";
+    do {
+        cout << "nの値:";
+        cin >> n;
+    } while (n <= 0);
+
+    int sum = 0;
+    int i = 1;
+    while (i <= n) {
+        sum += i;
+        i++;
+    }
+    cout << "1から" << n << "までの和は" << sum << "です。\n";
+}
