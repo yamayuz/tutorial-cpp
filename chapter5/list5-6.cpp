6d417dc32e7199141d61d1308c967b273aa9a57f
yamayuz <yuki@yamayulab.com>
2022-04-29 23:52:12 +0900

    [add] how to use array

---
+++ chapter5/list5-6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int ninzu = 5;
+    int tensu[ninzu];
+    int sum = 0;
+
+    cout << ninzu << "人の点数の合計点と平均点を求めます。";
+    for (int i = 0; i < ninzu; i++) {
+        cout << i + 1 <<"番の点数:";
+        cin >> tensu[i];
+        sum += tensu[i];
+    }
+
+    cout << "合計は" << sum << "点です。\n";
+    cout << "平均は" << static_cast<double>(sum) / ninzu << "点です。\n";
+}
